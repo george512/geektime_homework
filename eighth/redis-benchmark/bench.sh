da20ed92d3110b9f1d4c3659eb93023aa91a8f1c
george <756264852@qq.com>
2022-08-18 14:26:25 +0800

    add redis benmarktest result

--- eighth/redis-benchmark/bench.sh
+++ eighth/redis-benchmark/bench.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 
-redis-benchmark -t get,set -d 10 >> result_10
-redis-benchmark -t get,set -d 20 >> result_20
-redis-benchmark -t get,set -d 50 >> result_50
-redis-benchmark -t get,set -d 100 >> result_100
-redis-benchmark -t get,set -d 200 >> result_200
-redis-benchmark -t get,set -d 1000 >> result_1000
-redis-benchmark -t get,set -d 5000 >> result_5000
-
+redis-benchmark -t get,set -d 10 > result_10
+redis-benchmark -t get,set -d 20 > result_20
+redis-benchmark -t get,set -d 50 > result_50
+redis-benchmark -t get,set -d 100 > result_100
+redis-benchmark -t get,set -d 200 > result_200
+redis-benchmark -t get,set -d 1000 > result_1000
+redis-benchmark -t get,set -d 5000 > result_5000
+redis-benchmark -t get,set -d 10000 > result_10000
 
